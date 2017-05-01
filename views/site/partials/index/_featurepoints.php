af0ae5e3d537f16134a465ec37dee210c8ea937b
Ricardo O <robregonm@gmail.com>
2017-05-01 09:37:34 -0500

    Traducido "Feature Points"

--- views/site/partials/index/_featurepoints.php
+++ views/site/partials/index/_featurepoints.php
@@ -3,33 +3,33 @@
         <div class="col-md-4">
             <div class="media">
               <div class="media-left">
-                  <img class="media-object" src="<?= Yii::getAlias('@web/image/front/fast-80.png') ?>" alt="Yii is Fast">
+                  <img class="media-object" src="<?= Yii::getAlias('@web/image/front/fast-80.png') ?>" alt="Yii is Rápido">
               </div>
               <div class="media-body">
-                <div class="media-heading fast">Fast</div>
-                Yii gives you the maximum functionality by adding the least possible overhead
+                <div class="media-heading fast">Rápido</div>
+                Yii le proporciona la máxima funcionalidad añadiendo la menor carga posible
               </div>
             </div>
         </div>
         <div class="col-md-4">
             <div class="media">
               <div class="media-left">
-                    <img class="media-object" src="<?= Yii::getAlias('@web/image/front/secure-80.png') ?>" alt="Yii is Secure">
+                    <img class="media-object" src="<?= Yii::getAlias('@web/image/front/secure-80.png') ?>" alt="Yii es Seguro">
               </div>
               <div class="media-body">
-                <div class="media-heading secure">Secure</div>
-                    Sane defaults and built in tools helps you write solid and secure code
+                <div class="media-heading secure">Seguro</div>
+                    Lo sensatos valores por defecto y las herramientas incluidas le ayudan a escribir código sólido y seguro
               </div>
             </div>
         </div>
         <div class="col-md-4">
             <div class="media">
               <div class="media-left">
-                    <img class="media-object" src="<?= Yii::getAlias('@web/image/front/professional-80.png') ?>" alt="Yii is Efficient">
+                    <img class="media-object" src="<?= Yii::getAlias('@web/image/front/professional-80.png') ?>" alt="Yii es Eficiente">
               </div>
               <div class="media-body">
-                <div class="media-heading efficient">Efficient</div>
-                    Write more code in less time with simple, yet powerful APIs and code generation
+                <div class="media-heading efficient">Eficiente</div>
+                    Escriba más código en menor tiempo con APIs y generadores de código sencillos pero poderosos
               </div>
             </div>
         </div>
